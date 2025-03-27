d3a43db1fc8c4281765d2f6a31c44d06aed0e206
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-27 23:47:12 +0600

    services section added

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Button from "./Button";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
 
 export {
     Button,
     ShoeCard,
     PopularProductCard,
+    ServiceCard,
 }
